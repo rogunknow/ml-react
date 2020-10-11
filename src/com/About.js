89e7248a1c66a519a2077982daa0105f9dd70672
rogunknow <68379937+rogunknow@users.noreply.github.com>
2020-10-11 22:22:20 +0530

    Update About.js
    solved predicition issue

--- src/com/About.js
+++ src/com/About.js
@@ -1,38 +1,79 @@
 import React from "react";
-import init from './predict'
-import Predict from './predict'
 import * as tmImage from '@teachablemachine/image'
 import '@tensorflow/tfjs'
-import { OverlayTrigger, Popover, Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const URL = "https://teachablemachine.withgoogle.com/models/Qj55aZpKc/";
+
+let model, webcam, labelContainer, maxPredictions;
+
+export  async function init() {
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
+
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
+
+    const flip = true;
+    webcam = new tmImage.Webcam(200, 200, flip); 
+    await webcam.setup(); 
+    await webcam.play();
+    window.requestAnimationFrame(loop);
+
+   
+    document.getElementById("webcam-container").appendChild(webcam.canvas);
+    labelContainer = document.getElementById("label-container");
+    for (let i = 0; i < maxPredictions; i++) { 
+        labelContainer.appendChild(document.createElement("div"));
+    }
+}
+
+async function loop() {
+    webcam.update(); 
+    let ans = await predict();
+    if (!ans) {
+      window.requestAnimationFrame(loop);
+    }
+  
+    if ((ans === "you")) {
+      console.log("par");
+      window.location.href = "http://localhost:3000/"; 
+    }
+    else{
+        console.log("darsh");
+    }  
+    console.log(window);
+  }
+  
+
+  async function predict() {
+    const prediction = await model.predict(webcam.canvas);
+    console.log(prediction);
+  
+    let ans;
+  
+    for (let i = 0; i < maxPredictions; i++) {
+      const classPrediction =
+        prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+      labelContainer.childNodes[i].innerHTML = classPrediction;
+  
+      if (prediction[i].probability.toFixed(2) > 0.9) {
+        ans = prediction[i].className;
+      }
+    }
+  
+    return ans;
+  }
 
 function About() {
-  
   return (
-    <div className="home">
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <div id="webcam-container"></div>
-            <div id="label-container"></div>
-            <button type="button" onclick={init()}>Start</button>
-         
-          </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">Home</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
-
-        </div>
-      </div>
-    </div>
-    </div>
+    <>
+    <div>Image Model</div>
+    <button onclick={init()}/>
+<div id="webcam-container"></div>
+<div id="label-container"></div>
+   </>
   );
 }
- 
-export default About;
 
+export default About;
